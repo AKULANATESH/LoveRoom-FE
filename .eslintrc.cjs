2914dce5c958da841a630f2c212ba76953869590
Vikas Parsa <vikasparasa@gmail.com>
2024-06-17 23:41:10 +0530

    refactor: add prettier & eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,28 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  plugins: ["react-refresh", "simple-import-sort"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    /**
+     * React no longer needs to be in scope
+     */
+    "react/react-in-jsx-scope": "off",
+    "simple-import-sort/imports": "error",
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      {
+        fixStyle: "inline-type-imports",
+      },
     ],
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
   },
 };
